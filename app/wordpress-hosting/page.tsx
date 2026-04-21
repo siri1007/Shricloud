d472955dfb7a80697d4e437d58226fc8aa95e16b
siri1020 <siridundigala@gmail.com>
2026-04-21 17:20:32 +0530

    fix: disable eslint during build

--- app/wordpress-hosting/page.tsx
+++ app/wordpress-hosting/page.tsx
@@ -1,56 +1,56 @@
-"use client";
+// "use client";
 
-import { useRef } from "react";
+// import { useRef } from "react";
 
 
-import Header from "@/components/layout/header/Header";
-import WordpressBanner from "@/components/layout/banner/WordpressBanner";
-import HostingChooseTwo from "@/components/containers/hosting/HostingChooseTwo";
-import HomeThreePricing from "@/components/containers/home-three/HomeThreePricing";
-import HomeHostingTwo from "@/components/containers/home/HomeHostingTwo";
-import Testimonial from "@/components/containers/support/Testimonial";
-import Brand from "@/components/containers/support/Brand";
-import FaqSection from "@/components/containers/faq/FaqSection";
-import Footer from "@/components/layout/footer/Footer";
-import CustomCursor from "@/components/layout/CustomCursor";
+// import Header from "@/components/layout/header/Header";
+// import WordpressBanner from "@/components/layout/banner/WordpressBanner";
+// import HostingChooseTwo from "@/components/containers/hosting/HostingChooseTwo";
+// import HomeThreePricing from "@/components/containers/home-three/HomeThreePricing";
+// import HomeHostingTwo from "@/components/containers/home/HomeHostingTwo";
+// import Testimonial from "@/components/containers/support/Testimonial";
+// import Brand from "@/components/containers/support/Brand";
+// import FaqSection from "@/components/containers/faq/FaqSection";
+// import Footer from "@/components/layout/footer/Footer";
+// import CustomCursor from "@/components/layout/CustomCursor";
 
-const page = () => {
+// const page = () => {
 
-   const pricingRef = useRef<HTMLDivElement | null>(null);
+//    const pricingRef = useRef<HTMLDivElement | null>(null);
   
-    const scrollToPricing = () => {
-      pricingRef.current?.scrollIntoView({ behavior: "smooth" });
-    };
+//     const scrollToPricing = () => {
+//       pricingRef.current?.scrollIntoView({ behavior: "smooth" });
+//     };
   
-  return (
+//   return (
 
 
 
-    <>
-      <Header />
-      <WordpressBanner
-  scrollToPricing={() => {
-    document
-      .getElementById("pricing")
-      ?.scrollIntoView({ behavior: "smooth" });
-  }}
-/>
+//     <>
+//       <Header />
+//       <WordpressBanner
+//   scrollToPricing={() => {
+//     document
+//       .getElementById("pricing")
+//       ?.scrollIntoView({ behavior: "smooth" });
+//   }}
+// />
 
 
-      <HostingChooseTwo />
+//       <HostingChooseTwo />
 
-      <div id="pricing">
-  <HomeThreePricing />
-</div>
+//       <div id="pricing">
+//   <HomeThreePricing />
+// </div>
      
-      <HomeHostingTwo />
-      <Testimonial />
-      <Brand />
-      <FaqSection />
-      <Footer />
-      <CustomCursor />
-    </>
-  );
-};
+//       <HomeHostingTwo />
+//       <Testimonial />
+//       <Brand />
+//       <FaqSection />
+//       <Footer />
+//       <CustomCursor />
+//     </>
+//   );
+// };
 
-export default page;
+// export default page;
