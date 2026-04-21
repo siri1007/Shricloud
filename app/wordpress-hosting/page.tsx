8e95690d1eb1482600e9001cb966ffe2d44323e5
siri1020 <siridundigala@gmail.com>
2026-04-21 16:57:26 +0530

    fix: disable eslint during build

--- app/wordpress-hosting/page.tsx
+++ app/wordpress-hosting/page.tsx
@@ -1,31 +1,83 @@
+// import Header from "@/components/layout/header/Header";
+// // import WordpressBanner from "@/components/layout/banner/WordpressBanner";
+// import HostingChooseTwo from "@/components/containers/hosting/HostingChooseTwo";
+// import HomeThreePricing from "@/components/containers/home-three/HomeThreePricing";
+// import HomeHostingTwo from "@/components/containers/home/HomeHostingTwo";
+// import Testimonial from "@/components/containers/support/Testimonial";
+// import Brand from "@/components/containers/support/Brand";
+// import FaqSection from "@/components/containers/faq/FaqSection";
+// import Footer from "@/components/layout/footer/Footer";
+// import CustomCursor from "@/components/layout/CustomCursor";
+
+// const page = () => {
+//   return (
+
+
+//     <>
+//       <Header />
+//       {/* <WordpressBanner /> */}
+//       <HostingChooseTwo />
+//       <HomeThreePricing />
+//       <HomeHostingTwo />
+//       <Testimonial />
+//       <Brand />
+//       <FaqSection />
+//       <Footer />
+//       <CustomCursor />
+//     </>
+//   );
+// };
+
+// export default page;
+
+
+
+
+
+
+"use client";
+
+import { useRef } from "react";
+
 import Header from "@/components/layout/header/Header";
-// import WordpressBanner from "@/components/layout/banner/WordpressBanner";
-import HostingChooseTwo from "@/components/containers/hosting/HostingChooseTwo";
-import HomeThreePricing from "@/components/containers/home-three/HomeThreePricing";
-import HomeHostingTwo from "@/components/containers/home/HomeHostingTwo";
+import PricingChoose from "@/components/containers/pricing/PricingChoose";
 import Testimonial from "@/components/containers/support/Testimonial";
 import Brand from "@/components/containers/support/Brand";
-import FaqSection from "@/components/containers/faq/FaqSection";
 import Footer from "@/components/layout/footer/Footer";
 import CustomCursor from "@/components/layout/CustomCursor";
+import DataCenter from "@/components/DataCenter/DataCenter";
+import WordpressPricing from "@/components/containers/home-three/WordpressPricing";
+import WordpressBanner from "@/components/layout/banner/WordpressBanner";
+import StackCards from "@/components/StackCards/StackCards";
+import FaqSection from "@/components/containers/faq/WpFAQSection";
 
-const page = () => {
+const Page = () => {
+  const pricingRef = useRef<HTMLDivElement | null>(null);
+
+  const scrollToPricing = () => {
+    pricingRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-
-
     <>
       <Header />
-      {/* <WordpressBanner /> */}
-      <HostingChooseTwo />
-      <HomeThreePricing />
-      <HomeHostingTwo />
+
+      <WordpressBanner scrollToPricing={scrollToPricing} />
+
+      <div ref={pricingRef}>
+        <WordpressPricing />
+      </div>
+
+      <PricingChoose />
+      <StackCards />
+      <FaqSection />
+      <DataCenter />
       <Testimonial />
       <Brand />
-      <FaqSection />
       <Footer />
       <CustomCursor />
     </>
   );
 };
 
-export default page;
+export default Page;
