043ff1e33ba4d8cd80c1132cd3c03feca1b41a97
siri1020 <siridundigala@gmail.com>
2026-05-23 12:45:44 +0530

    modified home  page

--- components/containers/hosting/DomainList.tsx
+++ components/containers/hosting/DomainList.tsx
@@ -1,204 +1,255 @@
-import Image from "next/image";
-import one from "@/public/img/domain/online.png";
-import two from "@/public/img/domain/org.png";
-import three from "@/public/img/domain/com.png";
-import four from "@/public/img/domain/shop.png";
-import five from "@/public/img/domain/net.png";
-import six from "@/public/img/domain/pro.png";
-import seven from "@/public/img/domain/xyz.png";
-import eight from "@/public/img/domain/top.png";
+// import Image from "next/image";
+// import one from "@/public/img/domain/online.png";
+// import two from "@/public/img/domain/org.png";
+// import three from "@/public/img/domain/com.png";
+// import four from "@/public/img/domain/shop.png";
+// import five from "@/public/img/domain/net.png";
+// import six from "@/public/img/domain/pro.png";
+// import seven from "@/public/img/domain/xyz.png";
+// import eight from "@/public/img/domain/top.png";
 
-         const domains = [
+//          const domains = [
+//   ".com", ".in", ".co", ".net", ".org",
+//   ".club", ".info", ".xyz", ".io",
+//   ".us", ".uk", ".foundation", ".academy"
+// ];
+
+
+
+// const DomainList = () => {
+//   return (
+//     <section className="domain-list-section fix section-padding pt-0">
+//       <div className="container">
+//         <div className="section-title text-center pb-md-2">
+//           {/* <span
+//             className="style-border "
+//             data-aos="fade-up"
+//             data-aos-duration="800"
+//           >
+//             Domains list
+//           </span> */}
+//          <h2 className="mb-3 mt-5">
+//   Top Domains Price List
+// </h2>
+          
+//           {/* <p data-aos="fade-up" data-aos-duration="800" data-aos-delay="500">
+//             Aliquam viverra accumsan lectus in dignissim ante interdum eu Sed
+//             odio massa
+//           </p> */}
+
+//         </div>
+//       <div className="row g-0 justify-content-center align-items-center">
+//           <div
+//             className="col-xl-2 col-lg-4 col-md-6 "
+           
+//           >
+          
+   
+
+// <div className="domain-list-items">
+//   <h6 className="top-text">Name</h6>
+//   <ul>
+//     <li className="border-none">
+//       <span></span>
+//     </li>
+//     {domains.map((domain, index) => (
+//       <li key={index}>{domain}</li>
+//     ))}
+//   </ul>
+// </div>
+
+
+//           </div>
+//           <div
+//             className="col-xl-2 col-lg-4 col-md-6 "
+            
+//           >
+//             <div className="domain-list-items">
+//               <h6 className="top-text">Register</h6>
+//               <ul>
+//                 <li className="border-none">
+//                   <span></span>
+//                 </li>
+//                 <li className="text-center style-2">
+//                 ₹1,099.00 INR
+//                 </li>
+//                 <li className="text-center style-2">
+//                   ₹699.00 INR
+//                 </li>
+//                 <li className="text-center style-2">
+//                 ₹999.00 INR
+//                 </li>
+//                 <li className="text-center style-2">₹999.00 INR</li>
+//                 <li className="text-center style-2">
+//                  ₹899.00 INR
+//                 </li>
+//                 <li className="text-center style-2">₹499.00 INR</li>
+//                 <li className="text-center style-2">
+//                  ₹499.00 INR
+//                 </li>
+//                 <li className="text-center style-2">₹299.00 INR</li>
+//                 <li className="text-center style-2">₹3,899.00 INR</li>
+//                 <li className="text-center style-2">₹499.00  INR</li>
+//                 <li className="text-center style-2">₹699.00 INR</li>
+//  <li className="text-center style-2"> ₹1,399.00 INR</li>
+//   <li className="text-center style-2"> ₹1,499.00 INR</li>
+
+
+               
+//               </ul>
+//             </div>
+//           </div>
+//           <div
+//             className="col-xl-2 col-lg-4 col-md-6 "
+           
+//           >
+//             <div className="domain-list-items">
+//               <h6 className="top-text">Transfer</h6>
+//               <ul>
+//                 <li className="border-none">
+//                   <span></span>
+//                 </li>
+//                 <li className="text-center style-2">₹999.00 INR</li>
+//                 <li className="text-center style-2">₹699.00 INR</li>
+//                 <li className="text-center style-2">₹999.00 INR</li>
+//                 <li className="text-center style-2">₹1,099.00 INR</li>
+//                 <li className="text-center style-2">₹899.00 INR</li>
+//                 <li className="text-center style-2">₹1,299.00 INR
+// </li>
+//                 <li className="text-center style-2">₹1,899.00 INR</li>
+//                 <li className="text-center style-2">₹999.00 INR</li>
+
+//                 <li className="text-center style-2">₹3899.00 INR</li>
+
+
+//                 <li className="text-center style-2">₹799.00 INR</li>
+
+
+//                 <li className="text-center style-2">₹699.00 INR</li><li className="text-center style-2">₹2199.00 INR</li>
+
+
+//                 <li className="text-center style-2">₹3299.00 INR</li>
+
+               
+//               </ul>
+//             </div>
+//           </div>
+//           <div
+//             className="col-xl-2 col-lg-4 col-md-6 "
+            
+//           >
+//             <div className="domain-list-items">
+//               <h6 className="top-text">Renew</h6>
+//               <ul>
+//                 <li className="border-none">
+//                   <span></span>
+//                 </li>
+//                 <li className="text-center style-2">₹1,099.00 INR</li>
+//                 <li className="text-center style-2">₹799.00 INR</li>
+//                 <li className="text-center style-2">₹999.00 INR</li>
+//                 <li className="text-center style-2">₹1299.00 INR</li>
+//                 <li className="text-center style-2">₹999.00 INR</li>
+//                 <li className="text-center style-2">₹1299.00 INR</li>
+//                 <li className="text-center style-2">₹1999.00 INR</li>
+//                 <li className="text-center style-2">₹1,099.00 INR</li>
+//                 <li className="text-center style-2">₹3899.00 INR</li>
+//                 <li className="text-center style-2">₹799.00 INR</li>
+//                 <li className="text-center style-2">₹699.00 INR</li>
+// <li className="text-center style-2">₹2199.00 INR</li>
+// <li className="text-center style-2">₹3899.00 INR</li>
+
+
+//               </ul>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default DomainList;
+
+
+
+// -----------------------
+
+
+
+"use client";
+
+import styles from "./DomainList.module.scss";
+
+const domains = [
   ".com", ".in", ".co", ".net", ".org",
   ".club", ".info", ".xyz", ".io",
   ".us", ".uk", ".foundation", ".academy"
 ];
 
+const register = [
+  "₹1,099.00 INR", "₹699.00 INR", "₹999.00 INR", "₹999.00 INR",
+  "₹899.00 INR", "₹499.00 INR", "₹499.00 INR", "₹299.00 INR",
+  "₹3,899.00 INR", "₹499.00 INR", "₹699.00 INR",
+  "₹1,399.00 INR", "₹1,499.00 INR"
+];
 
+const transfer = [
+  "₹999.00 INR", "₹699.00 INR", "₹999.00 INR", "₹1,099.00 INR",
+  "₹899.00 INR", "₹1,299.00 INR", "₹1,899.00 INR", "₹999.00 INR",
+  "₹3,899.00 INR", "₹799.00 INR", "₹699.00 INR",
+  "₹2,199.00 INR", "₹3,299.00 INR"
+];
 
-const DomainList = () => {
+const renew = [
+  "₹1,099.00 INR", "₹799.00 INR", "₹999.00 INR", "₹1,299.00 INR",
+  "₹999.00 INR", "₹1,299.00 INR", "₹1,999.00 INR", "₹1,099.00 INR",
+  "₹3,899.00 INR", "₹799.00 INR", "₹699.00 INR",
+  "₹2,199.00 INR", "₹3,899.00 INR"
+];
+
+export default function DomainList() {
   return (
-    <section className="domain-list-section fix section-padding pt-0">
-      <div className="container">
-        <div className="section-title text-center pb-md-2">
-          {/* <span
-            className="style-border "
-            data-aos="fade-up"
-            data-aos-duration="800"
-          >
-            Domains list
-          </span> */}
-         <h2 className="mb-3 mt-5">
-  Top Domains Price List
-</h2>
-          
-          {/* <p data-aos="fade-up" data-aos-duration="800" data-aos-delay="500">
-            Aliquam viverra accumsan lectus in dignissim ante interdum eu Sed
-            odio massa
-          </p> */}
+    <section className={styles.section}>
+      <div className={styles.container}>
 
-        </div>
-      <div className="row g-0 justify-content-center align-items-center">
-          <div
-            className="col-xl-2 col-lg-4 col-md-6 "
-           
-          >
-            {/* <div className="domain-list-items">
-              <h6 className="top-text">Name</h6>
-              <ul>
-                <li className="border-none">
-                  <span></span>
-                </li>
-                <li>
-                  <Image src={one} alt="img" priority />
-                </li>
-                <li>
-                  <Image src={two} alt="img" priority />
-                </li>
-                <li>
-                  <Image src={three} alt="img" priority />
-                </li>
-                <li>
-                  <Image src={four} alt="img" priority />
-                </li>
-                <li>
-                  <Image src={five} alt="img" priority />
-                </li>
-                <li>
-                  <Image src={six} alt="img" priority />
-                </li>
-                <li>
-                  <Image src={seven} alt="img" priority />
-                </li>
-                <li>
-                  <Image src={eight} alt="img" priority />
-                </li>
-              </ul>
-            </div> */}
+        <h2 className={styles.title}>Top Domains Price List</h2>
 
+        <div className={styles.tableWrapper}>
 
-   
-
-<div className="domain-list-items">
-  <h6 className="top-text">Name</h6>
-  <ul>
-    <li className="border-none">
-      <span></span>
-    </li>
-    {domains.map((domain, index) => (
-      <li key={index}>{domain}</li>
-    ))}
-  </ul>
-</div>
-
-
+          {/* NAME */}
+          <div className={styles.column}>
+            <h6>Name</h6>
+            {domains.map((d, i) => (
+              <div key={i} className={styles.cell}>{d}</div>
+            ))}
           </div>
-          <div
-            className="col-xl-2 col-lg-4 col-md-6 "
-            
-          >
-            <div className="domain-list-items">
-              <h6 className="top-text">Register</h6>
-              <ul>
-                <li className="border-none">
-                  <span></span>
-                </li>
-                <li className="text-center style-2">
-                ₹1,099.00 INR
-                </li>
-                <li className="text-center style-2">
-                  ₹699.00 INR
-                </li>
-                <li className="text-center style-2">
-                ₹999.00 INR
-                </li>
-                <li className="text-center style-2">₹999.00 INR</li>
-                <li className="text-center style-2">
-                 ₹899.00 INR
-                </li>
-                <li className="text-center style-2">₹499.00 INR</li>
-                <li className="text-center style-2">
-                 ₹499.00 INR
-                </li>
-                <li className="text-center style-2">₹299.00 INR</li>
-                <li className="text-center style-2">₹3,899.00 INR</li>
-                <li className="text-center style-2">₹499.00  INR</li>
-                <li className="text-center style-2">₹699.00 INR</li>
- <li className="text-center style-2"> ₹1,399.00 INR</li>
-  <li className="text-center style-2"> ₹1,499.00 INR</li>
 
-
-               
-              </ul>
-            </div>
+          {/* REGISTER */}
+          <div className={styles.column}>
+            <h6>Register</h6>
+            {register.map((p, i) => (
+              <div key={i} className={styles.cell}>{p}</div>
+            ))}
           </div>
-          <div
-            className="col-xl-2 col-lg-4 col-md-6 "
-           
-          >
-            <div className="domain-list-items">
-              <h6 className="top-text">Transfer</h6>
-              <ul>
-                <li className="border-none">
-                  <span></span>
-                </li>
-                <li className="text-center style-2">₹999.00 INR</li>
-                <li className="text-center style-2">₹699.00 INR</li>
-                <li className="text-center style-2">₹999.00 INR</li>
-                <li className="text-center style-2">₹1,099.00 INR</li>
-                <li className="text-center style-2">₹899.00 INR</li>
-                <li className="text-center style-2">₹1,299.00 INR
-</li>
-                <li className="text-center style-2">₹1,899.00 INR</li>
-                <li className="text-center style-2">₹999.00 INR</li>
 
-                <li className="text-center style-2">₹3899.00 INR</li>
-
-
-                <li className="text-center style-2">₹799.00 INR</li>
-
-
-                <li className="text-center style-2">₹699.00 INR</li><li className="text-center style-2">₹2199.00 INR</li>
-
-
-                <li className="text-center style-2">₹3299.00 INR</li>
-
-               
-              </ul>
-            </div>
+          {/* TRANSFER */}
+          <div className={styles.column}>
+            <h6>Transfer</h6>
+            {transfer.map((p, i) => (
+              <div key={i} className={styles.cell}>{p}</div>
+            ))}
           </div>
-          <div
-            className="col-xl-2 col-lg-4 col-md-6 "
-            
-          >
-            <div className="domain-list-items">
-              <h6 className="top-text">Renew</h6>
-              <ul>
-                <li className="border-none">
-                  <span></span>
-                </li>
-                <li className="text-center style-2">₹1,099.00 INR</li>
-                <li className="text-center style-2">₹799.00 INR</li>
-                <li className="text-center style-2">₹999.00 INR</li>
-                <li className="text-center style-2">₹1299.00 INR</li>
-                <li className="text-center style-2">₹999.00 INR</li>
-                <li className="text-center style-2">₹1299.00 INR</li>
-                <li className="text-center style-2">₹1999.00 INR</li>
-                <li className="text-center style-2">₹1,099.00 INR</li>
-                <li className="text-center style-2">₹3899.00 INR</li>
-                <li className="text-center style-2">₹799.00 INR</li>
-                <li className="text-center style-2">₹699.00 INR</li>
-<li className="text-center style-2">₹2199.00 INR</li>
-<li className="text-center style-2">₹3899.00 INR</li>
 
-
-              </ul>
-            </div>
+          {/* RENEW */}
+          <div className={styles.column}>
+            <h6>Renew</h6>
+            {renew.map((p, i) => (
+              <div key={i} className={styles.cell}>{p}</div>
+            ))}
           </div>
+
         </div>
       </div>
     </section>
   );
-};
-
-export default DomainList;
+}
