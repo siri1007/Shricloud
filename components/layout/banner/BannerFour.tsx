45053c387dc1240e2eb507e53b96c7a004bb76a5
siri1020 <siridundigala@gmail.com>
2026-05-26 11:00:03 +0530

    modified home  page

--- components/layout/banner/BannerFour.tsx
+++ components/layout/banner/BannerFour.tsx
@@ -136,34 +136,39 @@ const BannerFour = () => {
                 Find the right domain name.
               </h6>
 
-              <form
-                action="#"
-                className={styles.domainInputForm}
-              >
-                <div className={styles.domainInput}>
-                  <input type="text" placeholder="domain.com" />
+          <form
+  action="#"
+  className={styles.domainInputForm}
+>
+  <div className={styles.domainInput}>
+    
+    <input type="text" placeholder="domain.com" />
 
-                 
+    <div className={styles.selectArea}>
+      <select name="cate" className="category">
+        <option value="1">.com</option>
+        <option value="1">.in</option>
+        <option value="1">.co</option>
+        <option value="1">.net</option>
+        <option value="1">.org</option>
+        <option value="1">.club</option>
+        <option value="1">.info</option>
+        <option value="1">.xyz</option>
+        <option value="1">.io</option>
+        <option value="1">.us</option>
+        <option value="1">.uk</option>
+        <option value="1">.foundation</option>
+        <option value="1">.academy</option>
+      </select>
+    </div>
 
-                  <div className={styles.selectArea}>
-                    <select name="cate" className="category">
-                      <option value="1">.com</option>
-                      <option value="1">.in</option>
-                      <option value="1">.co</option>
-                      <option value="1">.net</option>
-                      <option value="1">.org</option>
-                      <option value="1">.club</option>
-                      <option value="1">.info</option>
-                      <option value="1">.xyz</option>
-                      <option value="1">.io</option>
-                      <option value="1">.us</option>
-                      <option value="1">.uk</option>
-                      <option value="1">.foundation</option>
-                      <option value="1">.academy</option>
-                    </select>
-                  </div>
-                </div>
-              </form>
+    {/* SEARCH BUTTON */}
+    <button type="submit" className={styles.searchBtn}>
+      Search
+    </button>
+
+  </div>
+</form>
 
               <ul className={styles.domainList}>
                 <li>
